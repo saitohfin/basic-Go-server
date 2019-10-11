f8aabe232c27f77b63d56da0344abe3d94c8c33e
javier.garcia <javier.garcia@e2ycommerce.com>
2019-10-11 16:27:30 +0200

    Sort league

--- players_test.go
+++ players_test.go
@@ -145,27 +145,6 @@ func TestRecordingWinsAndRetrievingThem(t *testing.T) {
 
 func TestFileSystemStore(t *testing.T) {
 
-	t.Run("/league from a reader", func(t *testing.T) {
-		database, cleanDatabase := createTempFile(t, `[
-            {"Name": "Cleo", "Wins": 10},
-            {"Name": "Chris", "Wins": 33}]`)
-		defer cleanDatabase()
-
-		store, err := NewFileSystemPlayerStore(database)
-		assertNoError(t, err)
-		got := store.GetLeague()
-
-		want := []Player{
-			{"Cleo", 10},
-			{"Chris", 33},
-		}
-
-		assertLeague(t, got, want)
-
-		got = store.GetLeague()
-		assertLeague(t, got, want)
-	})
-
 	t.Run("/get player score", func(t *testing.T) {
 		database, cleanDatabase := createTempFile(t, `[
             {"Name": "Cleo", "Wins": 10},
@@ -210,6 +189,28 @@ func TestFileSystemStore(t *testing.T) {
 		got := store.GetPlayerScore("Pepper")
 		want := 1
 		assertScoreEquals(t, got, want)
+	})
+
+	t.Run("league sorted", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, `[
+        {"Name": "Cleo", "Wins": 10},
+        {"Name": "Chris", "Wins": 33}]`)
+		defer cleanDatabase()
+
+		store, _ := NewFileSystemPlayerStore(database)
+
+		got := store.GetLeague()
+
+		want := []Player{
+			{"Chris", 33},
+			{"Cleo", 10},
+		}
+
+		assertLeague(t, got, want)
+
+		// read again
+		got = store.GetLeague()
+		assertLeague(t, got, want)
 	})
 }
 
